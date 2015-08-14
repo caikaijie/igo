b4450f1ca06680d819011db24b0e3f65d290f75b
caikaijie <caikaijie@gmail.com>
2015-08-14 13:32:56 +0800

    add comments

--- httpchain/httpchain.go
+++ httpchain/httpchain.go
@@ -1,7 +1,7 @@
 package httpchain
 
-import(
-  "net/http"
+import (
+	"net/http"
 )
 
 type Chain []http.Handler
@@ -9,34 +9,34 @@ type Chain []http.Handler
 type NextFunc func()
 
 type Hook interface {
-  HookProc(http.ResponseWriter, *http.Request, NextFunc)
+	HookProc(http.ResponseWriter, *http.Request, NextFunc)
 }
 
 func (hc Chain) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  var run func(http.ResponseWriter, *http.Request, []http.Handler)
+	var run func(http.ResponseWriter, *http.Request, []http.Handler)
 
-  makeNext := func(w http.ResponseWriter, r *http.Request, remaining []http.Handler) NextFunc {
-    return func(){run(w, r, remaining)}
-  }
+	makeNext := func(w http.ResponseWriter, r *http.Request, remaining []http.Handler) NextFunc {
+		return func() { run(w, r, remaining) }
+	}
 
-  run = func(w http.ResponseWriter, r *http.Request, handlers []http.Handler){
-    for i, handler := range handlers {
-      if hook, ok := handler.(Hook); ok {
-        var remaining []http.Handler
-        if i < len(handlers) {
-          remaining = handlers[i + 1:]
-        }
-        hook.HookProc(w, r, makeNext(w, r, remaining))
-        return
-      }
+	run = func(w http.ResponseWriter, r *http.Request, handlers []http.Handler) {
+		for i, handler := range handlers {
+			if hook, ok := handler.(Hook); ok {
+				var remaining []http.Handler
+				if i < len(handlers) {
+					remaining = handlers[i+1:]
+				}
+				hook.HookProc(w, r, makeNext(w, r, remaining))
+				return
+			}
 
-      handler.ServeHTTP(w, r)
-    }
-  }
+			handler.ServeHTTP(w, r)
+		}
+	}
 
-  makeNext(w, r, []http.Handler(hc)[:])()
+	makeNext(w, r, []http.Handler(hc)[:])()
 }
 
 type DummyHandler struct{}
 
-func (* DummyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func (*DummyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
--- httpcontext/context.go
+++ httpcontext/context.go
@@ -5,11 +5,20 @@ import (
 	"net/http"
 )
 
+// ContextHandler introduces google context(https://blog.golang.org/context) into http.Handler
+// with Context, value can be easily shared between middlewares
+// and it is idiomatic to implement "Go Concurrency Patterns"
+// 
+// middlewares(implementing ContextHandler) are encouraged to provide two functions:
+// 1. context value can be easily shared
+//		FromContext(context.Context) AnyData
+// 2. error can be easily handled(https://blog.golang.org/errors-are-values)
+//		Err(context.Context) error
 type ContextHandler interface {
 	ServeHTTPWithContext(parent context.Context, w http.ResponseWriter, r *http.Request) context.Context
 }
 
-// MakeHandler
+// MakeHandler chains ContextHandlers as http.Handler
 func MakeHandler(parent context.Context, chs ...ContextHandler) http.Handler {
 	if parent == nil {
 		parent = context.Background()
